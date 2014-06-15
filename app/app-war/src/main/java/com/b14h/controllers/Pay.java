7a7c1a321a6341f071dd55981e5af1417af4bf24
oli <oliver.stadie@gmail.com>
2014-06-15 17:07:56 +0200

    First working verison of payment.

--- app/app-war/src/main/java/com/b14h/controllers/Pay.java
+++ app/app-war/src/main/java/com/b14h/controllers/Pay.java
@@ -1,9 +1,11 @@
 package com.b14h.controllers;
 
+import static com.google.appengine.api.datastore.FetchOptions.Builder.withLimit;
+
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -14,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Query;
 import com.paypal.exception.ClientActionRequiredException;
 import com.paypal.exception.HttpErrorException;
 import com.paypal.exception.InvalidCredentialException;
@@ -22,68 +25,73 @@ import com.paypal.exception.MissingCredentialException;
 import com.paypal.exception.SSLConfigurationException;
 import com.paypal.sdk.exceptions.OAuthException;
 import com.paypal.svcs.services.AdaptivePaymentsService;
-import com.paypal.svcs.types.ap.PreapprovalRequest;
-import com.paypal.svcs.types.ap.PreapprovalResponse;
+import com.paypal.svcs.types.ap.PayRequest;
+import com.paypal.svcs.types.ap.PayResponse;
+import com.paypal.svcs.types.ap.Receiver;
+import com.paypal.svcs.types.ap.ReceiverList;
 import com.paypal.svcs.types.common.RequestEnvelope;
 
-public class Preapproval extends HttpServlet {
+public class Pay extends HttpServlet {
 
-	private static final long serialVersionUID = -5608710629269370689L;
+	private static final long serialVersionUID = 2187633796363073807L;
 
 	private final DatastoreService datastore = DatastoreServiceFactory
 			.getDatastoreService();
 
 	/**
-	 * Get Preapproval redirect URL.
+	 * TODO: use put requests instead of get requests. triggers a payment from a
+	 * static parent paypal account to a static store payment account.
 	 * 
 	 * @param req
-	 *            HttpServletRequest should contain the parent Paypal ID in
-	 *            future versions.
+	 *            HttpServletRequest
 	 * @param resp
-	 *            HttpServletResponse contains redirect URL to paypal with the
-	 *            preapproval key.
+	 *            HttpServletResponse
 	 * @throws ServletException
 	 * @throws IOException
 	 */
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		// super.doGet(req, resp);
-		resp.setContentType("text/html; charset=UTF-8");
-		resp.getWriter().write("Hello Preapproval!");
+		resp.getWriter().write("hello payment!");
+		
+		RequestEnvelope env = new RequestEnvelope();
+		env.setErrorLanguage("en_US");
+
+		List<Receiver> receiver = new ArrayList<Receiver>();
+		Receiver rec = new Receiver();
+		rec.setAmount(2.0);
+		rec.setEmail("teambh.store@gmx.de");
+		receiver.add(rec);
+		ReceiverList receiverlst = new ReceiverList(receiver);
+
+        Query q = new Query("preapprovalKey");
+        List<Entity> preapprovalKeys = datastore.prepare(q).asList(withLimit(50));
+        Entity entity = preapprovalKeys.get(0);
+        String preapprovalKey = (String) entity.getProperty("key");
+		
+		PayRequest payRequest = new PayRequest();
+		payRequest.setActionType("PAY");
+		payRequest.setReceiverList(receiverlst);
+		payRequest.setCurrencyCode("EUR");
+		payRequest.setCancelUrl("http://www.notUsedButRequiredUrl.de/");
+		payRequest.setReturnUrl("http://www.notUsedButRequiredUrl.de/");
+		payRequest.setRequestEnvelope(env);
+//		payRequest.setSenderEmail("teambh.parent@gmx.de");
+		payRequest.setPreapprovalKey(preapprovalKey);
 
 		Map<String, String> customConfigurationMap = new HashMap<String, String>();
-		customConfigurationMap.put("mode", "sandbox"); // Load the map with all
-														// mandatory parameters
+		customConfigurationMap.put("mode", "sandbox"); // Load the map with all mandatory parameters
 		customConfigurationMap.put("acct1.UserName", "teambh_api1.gmx.de");
 		customConfigurationMap.put("acct1.Password", "1402429072");
 		customConfigurationMap.put("acct1.Signature",
 				"AFcWxV21C7fd0v3bYYYRCpSSRl31AUsTqNigADQwIjFRV.1kjkOVHgdp");
 		// The Sandbox uses a global test App ID value that remains constant
 		customConfigurationMap.put("acct1.AppId", "APP-80W284485P519543T");
-
-		AdaptivePaymentsService adaptivePaymentsService = new AdaptivePaymentsService(
-				customConfigurationMap);
-
-		RequestEnvelope requestEnvelope = new RequestEnvelope("en_US");
-
-		Date now = new Date();
-		SimpleDateFormat sdfDestination = new SimpleDateFormat(
-				"yyyy-MM-dd'T'HH:mm:ss'Z'");
-		String startingDate = sdfDestination.format(now);
-
-		PreapprovalRequest preapprovalRequest = new PreapprovalRequest(
-				requestEnvelope,
-				"http://localhost:8080/html/canceledPreapproval/", "EUR",
-				"http://localhost:8080/html/succeededPreapproval/",
-				startingDate);
-
-		// String apiUserName = "randomApiUserName5489ht5njr89";
-
-		PreapprovalResponse preapprovalResponse = null;
+		AdaptivePaymentsService adaptivePaymentsService = new AdaptivePaymentsService(customConfigurationMap);
+		
+		PayResponse payResponse = null;
 		try {
-			preapprovalResponse = adaptivePaymentsService
-					.preapproval(preapprovalRequest);
+			payResponse = adaptivePaymentsService.pay(payRequest);
 		} catch (SSLConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -109,16 +117,8 @@ public class Preapproval extends HttpServlet {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		String preapprovalKey = preapprovalResponse.getPreapprovalKey();
-
-		// store the preapproval key
-		// TODO: temp solution until data models are implemented:
-		Entity keyEntity = new Entity("preapprovalKey");
-		keyEntity.setProperty("key", preapprovalKey);
-		datastore.put(keyEntity);
-
-		resp.sendRedirect("https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_ap-preapproval&preapprovalkey="
-				+ preapprovalKey);
+		
+		resp.getWriter().write("payment executed!");
 	}
+
 }
