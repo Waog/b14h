ffbe0e4b3a49ba8b1a95339423b2f0b86e1101b0
Timor <timor@cyhex.com>
2014-06-15 18:48:42 +0200

    populate tsk

--- app/app-war/src/main/java/com/b14h/controllers/PopulateServlet.java
+++ app/app-war/src/main/java/com/b14h/controllers/PopulateServlet.java
@@ -1,22 +1,16 @@
 package com.b14h.controllers;
 
-import com.b14h.libs.Constants;
-import com.b14h.libs.Constants.TaskStatus;
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
+import com.b14h.model.Task;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
+import static com.googlecode.objectify.ObjectifyService.ofy;
 
 public class PopulateServlet extends HttpServlet {
-
-    private final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
@@ -26,17 +20,11 @@ public class PopulateServlet extends HttpServlet {
                 + "rendering complicated bits of HTML from JSON data sources. Template functions can both interpolate "
                 + "variables, using <%= … %>, as well as execute arbitrary JavaScript code";
 
-        List<Entity> taskCollection = new ArrayList<Entity>();
-
         for (int i = 0; i < 10; i++) {
-            Entity task = new Entity(Constants.TASK_ENTITY);
-            task.setProperty("title", title + Integer.toString(i));
-            task.setProperty("description", desc + Integer.toString(i));
-            task.setProperty("credit", 10 + i);
-            task.setProperty("state", TaskStatus.OPEN);
-            taskCollection.add(task);
+            Task task = new Task(title, desc, 10 + 1);
+            task.setStatus(Task.TaskStatus.OPEN);
+            ofy().save().entities(task).now();
         }
-        datastore.put(taskCollection);
         resp.getWriter().write("OK");
     }
 }
--- app/app-war/src/main/java/com/b14h/model/Task.java
+++ app/app-war/src/main/java/com/b14h/model/Task.java
@@ -6,46 +6,51 @@
 
 package com.b14h.model;
 
-import com.b14h.libs.Constants.TaskStatus;
-import com.googlecode.objectify.annotation.Id;
 import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
 
 /**
- *
  * @author aabarb
  */
-@Entity(name="task")
+@Entity(name = "task")
 public class Task {
-    
+
     private final String description;
     private final int credit;
-    @Id private long taskId;
     private final String title;
+    @Id
+    private long taskId;
     private TaskStatus status;
-    
-    public String getTitle() {
-        return this.title;
-    }
-    
-    public String getDescription() {
-        return this.description;
-    }
-    
-    public int getCredit() {
-        return this.credit;
-    }
-    
-    public TaskStatus getStatus() {
-        return this.status;
-    }        
-    
+
     public Task(String title, String description, int credit) {
         this.title = title;
         this.description = description;
         this.credit = credit;
     }
-    
-    public void updateStatus(TaskStatus newStatus) {
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public int getCredit() {
+        return this.credit;
+    }
+
+    public TaskStatus getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(TaskStatus newStatus) {
         this.status = newStatus;
-    }    
+    }
+
+    public enum TaskStatus {
+        OPEN,
+        CLOSED,
+        CONFIRMED
+    }
 }
