fa179f0a22bf85a074985aae84a1f04cd49369aa
Timor <timor@cyhex.com>
2014-06-17 13:16:40 +0200

    line wrap

--- server/mon/monitor.py
+++ server/mon/monitor.py
@@ -4,7 +4,7 @@ __author__ = 'gx'
 import sys
 import time
 
-from lib.lcd import LCDSysInfo, BackgroundColours, TextLines, TextAlignment, TextColours
+from lib.lcd import LCDSysInfo, BackgroundColours, TextColours, TextLines
 
 
 lcd = LCDSysInfo()
@@ -12,18 +12,14 @@ lcd.set_text_background_colour(BackgroundColours.BLACK)
 lcd.set_brightness(255)
 lcd.dim_when_idle(False)
 
-lines = []
-
 while True:
     line = sys.stdin.readline().strip()
 
     if not line:
         time.sleep(0.1)
         continue
+    lcd.clear_lines(TextLines.ALL, BackgroundColours.BLACK)
+    time.sleep(0.1)
+    lcd.display_text_anywhere(0, 0, line[0:100], TextColours.GREEN)
+    time.sleep(0.1)
 
-    lines.append(line)
-    if len(lines) > 6:
-        lines.pop(0)
-
-    for i, l in enumerate(lines):
-        lcd.display_text_on_line(i+1, l, pad_for_icon=False, alignment=TextAlignment.LEFT, colour=TextColours.GREEN)
